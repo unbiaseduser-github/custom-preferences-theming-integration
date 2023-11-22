2e82fce70211f32db7f2a6c4fe34ac0302e34759
bruh <quangtrung02hn16@gmail.com>
2023-11-22 10:29:07 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://jitpack.io")
+    }
+}
+
+rootProject.name = "library-integrations"
+include(":custom-preferences-theming-integration")
+include(":theming-preference-integration")
