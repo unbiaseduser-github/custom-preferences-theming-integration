a77fd9cd61bbae867c17745320ffe9e4092c22d7
bruh <quangtrung02hn16@gmail.com>
2024-08-11 10:41:17 +0700

    Bump version

--- custom-preferences-theming-integration/build.gradle.kts
+++ custom-preferences-theming-integration/build.gradle.kts
@@ -47,7 +47,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.sixtyninefourtwenty"
                 artifactId = "custom-preferences-theming-integration"
-                version = "1.2.0"
+                version = "2.0.0"
 
                 from(components["release"])
 
