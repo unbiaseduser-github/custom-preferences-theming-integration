f6e59aa0ea9e71eaca829c95b7c25ccd06ae3e81
bruh <quangtrung02hn16@gmail.com>
2024-08-25 23:02:37 +0700

    Remove proguard files

--- custom-preferences-theming-integration/build.gradle.kts
+++ custom-preferences-theming-integration/build.gradle.kts
@@ -19,17 +19,6 @@ android {
         minSdk = 21
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
     }
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
