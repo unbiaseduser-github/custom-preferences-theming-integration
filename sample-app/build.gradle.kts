622863ff1ee9d07dc9ef534c90991e6fb82deb3d
bruh <quangtrung02hn16@gmail.com>
2024-09-04 16:40:41 +0700

    Bump custom-preferences and other stuff

--- sample-app/build.gradle.kts
+++ sample-app/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.preference:preference-ktx:1.2.1")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
