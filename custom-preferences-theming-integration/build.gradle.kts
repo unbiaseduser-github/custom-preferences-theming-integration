b59dceedde4b7335b66dbd3af18297d64772dcd9
bruh <quangtrung02hn16@gmail.com>
2024-10-04 13:30:44 +0700

    Bump `theming`

--- custom-preferences-theming-integration/build.gradle.kts
+++ custom-preferences-theming-integration/build.gradle.kts
@@ -82,7 +82,7 @@ afterEvaluate {
 
 dependencies {
 
-    api("com.github.unbiaseduser-github:theming:2.3.2")
+    api("com.github.unbiaseduser-github:theming:2.3.3")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
